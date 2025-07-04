d90dd55fbdf3b46836bcd8372f0ec641f17f7215
Hitesh1984 <hpdave1984@gmail.com>
2025-07-04 16:17:31 -0500

    Added new Testcases

--- Tests/SwiftLeetCodeAlgorithmsTests/LeetcodeArrayTests.swift
+++ Tests/SwiftLeetCodeAlgorithmsTests/LeetcodeArrayTests.swift
@@ -38,19 +38,22 @@ final class LeetcodeArrayTests: XCTestCase {
     }
     
     func testCandy1() throws {
-        // [1,2,3,4,5], cost = [3,4,5,1,2]
-        var res1 = [true,false,true]
+        let res1 = [true,false,true]
         XCTAssertEqual(array.kidsWithCandies([12,1,12], 10), res1)
-        var res2 = [true,true,true,false,true]
+        let res2 = [true,true,true,false,true]
         XCTAssertEqual(array.kidsWithCandies([2,3,5,1,3], 3), res2)
-        var res3 = [true,false,false,false,false]
+        let res3 = [true,false,false,false,false]
         XCTAssertEqual(array.kidsWithCandies([4,2,1,1,2], 1), res3)
     }
     
     func testCandy2() throws {
-        
         XCTAssertEqual(array.candy([1,0,2]),5)
         XCTAssertEqual(array.candy([1,2,2]),4)
+    }
+    
+    func testFlipgame() throws {
+        XCTAssertEqual(array.flipgame([1,2,4,4,7], [1,3,4,1,3]), 2)
+        XCTAssertEqual(array.flipgame([1], [1]), 0)
     }
     
     
